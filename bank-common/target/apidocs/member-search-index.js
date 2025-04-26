0ba1355e543bd781aa7ba51b05004ffad229ac6e
oğuz alp çepni <oguzalpcepni@gmail.com>
2025-04-26 19:50:51 +0300

    common paketi

---
+++ bank-common/target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"io.github.oguzalpcepni","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"io.github.oguzalpcepni","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ user-service/src/test/java/com/bankmanagement/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankmanagement.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
