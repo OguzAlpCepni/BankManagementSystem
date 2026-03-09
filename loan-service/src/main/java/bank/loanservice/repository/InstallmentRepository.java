12220012b2190581f921d3ab0b7ceefe68682e7b
oğuz alp çepni <oguzalpcepni@gmail.com>
2026-03-09 12:21:34 +0300

    deneme testi

--- loan-service/src/main/java/bank/loanservice/repository/InstallmentRepository.java
+++ loan-service/src/main/java/bank/loanservice/repository/InstallmentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-//kalem
+//sdjfhgasdhfajsdfhsdf
 public interface InstallmentRepository extends JpaRepository<Installment, UUID> {
 }
